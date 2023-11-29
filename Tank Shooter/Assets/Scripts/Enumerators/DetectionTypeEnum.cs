e95d527242cf9d01142ed28fa4d7bff6978eea72
Facundo <facuwilli@gmail.com>
2023-11-29 14:52:40 -0300

    Updated detection type enum.

--- Tank Shooter/Assets/Scripts/Enumerators/DetectionTypeEnum.cs
+++ Tank Shooter/Assets/Scripts/Enumerators/DetectionTypeEnum.cs
@@ -1,12 +1,15 @@
 using System;
 
+//Enums used in another of my projects
+//Link: https://github.com/lobinuxsoft/weather-guardian/blob/development/Assets/Scripts/Utils/DetectionType.cs
+
 namespace TankGame.Enumerators 
 {
     [Flags]
     public enum DetectionTypeEnum
     {
-        ENTER = 1,
-        STAY = 2,
-        EXIT = 3
+        ENTER = 1 << 0,
+        STAY = 1 << 1,
+        EXIT = 1 << 2
     }
 }
