0eb5952d94d2efad729c6590ebaaab30c1074892
Facundo <facuwilli@gmail.com>
2023-11-29 23:14:37 -0300

    Added movement to the projectile when activated.

--- Tank Shooter/Assets/Scripts/Projectile/ProjectileCore.cs
+++ Tank Shooter/Assets/Scripts/Projectile/ProjectileCore.cs
@@ -4,14 +4,21 @@ using TankGame.Time;
 
 namespace TankGame.Projectile
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class ProjectileCore : MonoBehaviour
     {
         [SerializeField] [Range(1.0f, 10.0f)] private float projectileLifeTime;
+
+        [SerializeField] [Range(10.0f, 100.0f)] private float projectileVelocity;
+
+        private Rigidbody thisRigidBody;
 
         private Timer lifeTimeTimer;
 
         private void Awake()
         {
+            thisRigidBody = GetComponent<Rigidbody>();            
+
             lifeTimeTimer = new Timer(projectileLifeTime, true, false);
         }
 
@@ -34,6 +41,10 @@ namespace TankGame.Projectile
         public void Activate(Transform launcherTransform) 
         {
             gameObject.SetActive(true);
+
+            thisRigidBody.MovePosition(launcherTransform.position);
+
+            thisRigidBody.velocity = launcherTransform.forward * projectileVelocity;
 
             lifeTimeTimer.ResetTime();
         }
