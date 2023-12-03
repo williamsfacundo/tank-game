aab7de486a6446f77b8be26434d1547f6ba7463c
Facundo <facuwilli@gmail.com>
2023-12-03 17:07:20 -0300

    Updated sphere jump behaviour script.

--- Tank Shooter/Assets/Scripts/Sphere/Movement/SphereJump.cs
+++ Tank Shooter/Assets/Scripts/Sphere/Movement/SphereJump.cs
@@ -1,25 +1,57 @@
+using TankGame.ScriptableObjects;
 using UnityEngine;
 
 namespace TankGame.Sphere.Movement 
 {
     public class SphereJump : ISphereMoveBehaviour
     {
+        private const float additionalSecuritySpace = 0.15f;
+
         private Rigidbody sphereRigidBody;
 
+        private Transform sphereBottomTransform;
+
+        private Ray rayToFloor;
+
+        private LayerMask floorLayerMask;
+        
         private float sphereJumpForce;
 
-        public SphereJump(Rigidbody sphereRigidBody, float jumpForce) 
+        private float floorDistanceToTriggerJump;
+
+        private float distanceToFloor;
+
+        public SphereJump(Rigidbody sphereRigidBody, Transform sphereBottomTransform,SphereParametersScriptableObject sphereStats) 
         {
             this.sphereRigidBody = sphereRigidBody;
 
-            sphereJumpForce = jumpForce;
+            this.sphereBottomTransform = sphereBottomTransform;
+
+            rayToFloor = new Ray(sphereBottomTransform.position, Vector3.down);
+
+            this.floorLayerMask = sphereStats.FloorLayerMask;
+
+            this. sphereJumpForce = sphereStats.SphereJumpForce;
+
+            this.floorDistanceToTriggerJump = sphereStats.FloorDistanceToTriggerJump;
+
+            distanceToFloor = 0.0f;
         }
 
         public void MoveBehaviour()
         {
-            if (sphereRigidBody.velocity.y <= 0.0f) 
+            rayToFloor.origin = sphereBottomTransform.position;
+
+            if (Physics.Raycast(rayToFloor, out RaycastHit hit, Mathf.Infinity, floorLayerMask)) 
             {
-                sphereRigidBody.AddForce(Vector3.up * sphereJumpForce, ForceMode.Acceleration);
+                distanceToFloor = hit.distance;
+            }
+
+            if (distanceToFloor <= floorDistanceToTriggerJump) 
+            {
+                sphereRigidBody.MovePosition(sphereRigidBody.transform.position + new Vector3(0.0f, Mathf.Abs(distanceToFloor - floorDistanceToTriggerJump) + additionalSecuritySpace, 0.0f));
+
+                sphereRigidBody.AddForce(Vector3.up * sphereJumpForce, ForceMode.Impulse);
             }
         }
     }
