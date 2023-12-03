57a8cd189ce1b025d9f3c59afc375f8a74be03df
Facundo <facuwilli@gmail.com>
2023-12-03 17:44:16 -0300

    Added sphere health script.

---
+++ Tank Shooter/Assets/Scripts/Sphere/SphereHealth.cs
@@ -0,0 +1,37 @@
+using System;
+
+using UnityEngine;
+
+using TankGame.Interfaces;
+
+public class SphereHealth : MonoBehaviour, IDamaged
+{
+    public static event Action OnSphereDestroyed;
+
+    private static int activeSphereInstances = 0;
+    
+    public static int ActiveSphereInstances
+    {
+        get 
+        {
+            return activeSphereInstances;
+        }
+    }    
+
+    private void OnEnable()
+    {
+        activeSphereInstances++;
+    }
+
+    private void OnDisable()
+    {
+        activeSphereInstances--;
+    }
+
+    public void DamageReceived()
+    {
+        OnSphereDestroyed?.Invoke();
+
+        gameObject.SetActive(false);
+    }
+}
