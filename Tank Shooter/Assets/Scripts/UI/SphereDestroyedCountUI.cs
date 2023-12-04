2deced30a610a3333440778e139da89fcc0504c1
Facundo <facuwilli@gmail.com>
2023-12-04 11:09:59 -0300

    Update sphere count UI script to track spheres destroyed.

---
+++ Tank Shooter/Assets/Scripts/UI/SphereDestroyedCountUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+using TMPro;
+using TankGame.Tank.Score;
+
+namespace TankGame.UI 
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class SphereDestroyedCountUI : MonoBehaviour
+    {
+        private TextMeshProUGUI sphereText;
+
+        private TankScoreBehaviour tankScoreBehaviour;
+
+        private void Awake()
+        {
+            sphereText = GetComponent<TextMeshProUGUI>();
+
+            tankScoreBehaviour = GameObject.FindGameObjectWithTag("Player").GetComponent<TankScoreBehaviour>();
+        }
+
+        private void Start()
+        {
+            UpdateSphereText();
+
+            tankScoreBehaviour.OnTankScoreChanged += UpdateSphereText;
+        }
+
+        private void OnDestroy()
+        {
+            tankScoreBehaviour.OnTankScoreChanged -= UpdateSphereText;
+        }
+
+        private void UpdateSphereText()
+        {
+            sphereText.text = TankScoreBehaviour.SpheresDestroyed.ToString();
+        }
+    }
+}
