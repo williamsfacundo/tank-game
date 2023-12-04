aa0a2daec86568e2e1a096937af0c6688d6a8188
Facundo <facuwilli@gmail.com>
2023-12-04 11:20:31 -0300

    Added funtionality to trigger end game when end game timer ends.

--- Tank Shooter/Assets/Scripts/Gameplay/GameplayEndGameTrigger.cs
+++ Tank Shooter/Assets/Scripts/Gameplay/GameplayEndGameTrigger.cs
@@ -4,16 +4,28 @@ using TankGame.Scenes;
 
 namespace TankGame.Gameplay 
 {
+    [RequireComponent(typeof(GameplayTimer))]
     public class GameplayEndGameTrigger : MonoBehaviour
     {
+        private GameplayTimer gameplayTimer;
+
+        private void Awake()
+        {
+            gameplayTimer = GetComponent<GameplayTimer>();
+        }
+
         private void Start()
         {
             SphereHealth.OnAllSpheresDisabled += TriggerEndGame;
+
+            gameplayTimer.endGameTimer.OnTimerEnds += TriggerEndGame;
         }
 
         private void OnDestroy()
         {
             SphereHealth.OnAllSpheresDisabled -= TriggerEndGame;
+
+            gameplayTimer.endGameTimer.OnTimerEnds -= TriggerEndGame;
         }
 
         private void TriggerEndGame() 
