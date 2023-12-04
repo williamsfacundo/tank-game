d7a409bf40a6c824703f436f8bb58e0f4f268355
Facundo <facuwilli@gmail.com>
2023-12-03 22:12:41 -0300

    Added tank score UI.

---
+++ Tank Shooter/Assets/Scripts/UI/TankScoreUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+using TMPro;
+
+using TankGame.Tank.Score;
+
+namespace TankGame.UI 
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TankScoreUI : MonoBehaviour
+    {
+        private TextMeshProUGUI tankScoreText;
+
+        private TankScoreBehaviour tankScoreBehaviour;
+
+        private void Awake()
+        {
+            tankScoreText = GetComponent<TextMeshProUGUI>();
+
+            tankScoreBehaviour = GameObject.FindGameObjectWithTag("Player").GetComponent<TankScoreBehaviour>();            
+        }
+
+        private void Start()
+        {
+            UpdateTankScoreText();
+
+            tankScoreBehaviour.OnTankScoreChanged += UpdateTankScoreText;
+        }
+
+        private void OnDestroy()
+        {
+            tankScoreBehaviour.OnTankScoreChanged -= UpdateTankScoreText;
+        }
+
+        private void UpdateTankScoreText()
+        {
+            tankScoreText.text = tankScoreBehaviour.TankScore.ToString();
+        }
+    }
+}
