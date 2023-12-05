2e40164d46e87f832a92f019ff88a3a414177d09
Facundo <facuwilli@gmail.com>
2023-12-05 15:01:20 -0300

    Added high score texts UI setters behaviour.

---
+++ Tank Shooter/Assets/Scripts/UI/HighScoreTextsUI.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+using TMPro;
+
+using TankGame.EndGame;
+
+namespace TankGame.UI
+{
+    [RequireComponent(typeof(EndGameHighScoreManager))]
+    public class HighScoreTextsUI : MonoBehaviour
+    {
+        private EndGameHighScoreManager highScoreManager;
+
+        [SerializeField] private TextMeshProUGUI[] highScoreTexts;
+
+        bool areEnoughTexts;
+
+        private void Awake()
+        {
+            areEnoughTexts = false;
+
+            highScoreManager = GetComponent<EndGameHighScoreManager>();
+
+            highScoreManager.OnHighScoreChanged += UpdateHighScoreTexts;            
+
+            if (highScoreTexts == null) 
+            {
+                Debug.LogError("No high score texts were assigned!");
+            }
+            else if (highScoreTexts.Length != 3) 
+            {
+                Debug.LogError("3 high score text must be assigned! Currently there are only " + highScoreTexts.Length);
+            }
+            else 
+            {
+                areEnoughTexts = true;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            highScoreManager.OnHighScoreChanged -= UpdateHighScoreTexts;
+        }
+
+        private void UpdateHighScoreTexts()
+        {
+            if (highScoreManager != null && areEnoughTexts) 
+            {
+                highScoreTexts[0].text = highScoreManager.HighScore1.ToString();
+
+                highScoreTexts[1].text = highScoreManager.HighScore2.ToString();
+
+                highScoreTexts[2].text = highScoreManager.HighScore3.ToString();
+            }
+        }
+    }
+}
